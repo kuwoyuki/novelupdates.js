721a0e32fddad11a9953237326cc2a4c0338ebf5
kuwoyuki <kuwoyuki@cock.li>
2019-06-15 00:26:53 +0200

    fixed licensed property, added completelyTranslated bool

--- tests/tagsList.test.js
+++ tests/tagsList.test.js
@@ -2,24 +2,20 @@ const { promises: fs } = require("fs");
 const { join } = require("path");
 const { tagsList } = require("../lib/parsers");
 
-describe("tags tests", () => {
-  test("parses the tag list", async () => {
-    const html = await fs.readFile(
-      join(__dirname, "fixtures", "tagsList.html")
-    );
-    const tags = await tagsList(html);
+test("parses the tag list", async () => {
+  const html = await fs.readFile(join(__dirname, "fixtures", "tagsList.html"));
+  const tags = await tagsList(html);
 
-    expect(tags.data).toHaveLength(50);
-    expect(tags.data).toEqual(
-      expect.arrayContaining([
-        {
-          tag: "Calm Protagonist",
-          description:
-            "This tag is used when the protagonist is able to remain calm in difficult and dangerous situation."
-        }
-      ])
-    );
-    expect(tags.pageInfo.current).toBe(3);
-    expect(tags.pageInfo.last).toBe(16);
-  });
+  expect(tags.data).toHaveLength(50);
+  expect(tags.data).toEqual(
+    expect.arrayContaining([
+      {
+        tag: "Calm Protagonist",
+        description:
+          "This tag is used when the protagonist is able to remain calm in difficult and dangerous situation."
+      }
+    ])
+  );
+  expect(tags.pageInfo.current).toBe(3);
+  expect(tags.pageInfo.last).toBe(16);
 });
